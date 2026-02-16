fbcda8e9adcb4c3dd8c7438f42916d947f499c87
Muhammad Haris <muhammadharis31001@gmail.com>
2026-02-16 21:10:46 +0500

    Create Day2.py
    This is only Practice Questions but After this we will start Theory with practice

---
+++ Day2.py
@@ -0,0 +1,27 @@
+#Practice Questions
+#********Take 3 numbers from user → calculate sum & average → print result******
+
+#num1=float(input("Enter first Value: "))
+#num2=float(input("Enter second Value: "))
+#num3=float(input("Enter third Value: "))
+#sum=num1+num2+num3
+#print("The Sum of your Values is: " ,sum)
+
+#num1=float(input("Enter first Value: "))
+#num2=float(input("Enter second Value: "))
+#num3=float(input("Enter third Value: "))
+#avg=(num1+num2+num3)/3
+#print("The Avrage of your Values is: " ,avg)
+
+#*********Take length & width → calculate area of rectangle → print result *******
+
+#Lenght=float(input("Enter Lenght Value: "))
+#Width=float(input("Enter Width Value: "))
+#area =Lenght*Width
+#print("Area of rectangle is: " ,area)
+
+#***************Take radius → calculate area of circle (use π = 3.14)*************
+
+#radious=float(input("Enter the Radious: "))
+#area =radious*3.14159
+#print("Area of Circle is: " ,area)
