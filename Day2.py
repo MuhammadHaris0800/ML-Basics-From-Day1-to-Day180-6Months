12228cd590c4c33feaea768ac753a80806c44e17
Muhammad Haris <muhammadharis31001@gmail.com>
2026-02-16 17:02:55 +0000

    Day2

--- Day2.py
+++ Day2.py
@@ -20,7 +20,7 @@
 #area =Lenght*Width
 #print("Area of rectangle is: " ,area)
 
-#***8*Take radius → calculate area of circle (use π = 3.14)*
+#****Take radius → calculate area of circle (use π = 3.14)*
 
 #radious=float(input("Enter the Radious: "))
 #area =radious*3.14159
