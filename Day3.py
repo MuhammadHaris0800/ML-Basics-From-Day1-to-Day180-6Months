d7634932a8891287ca3f84cc1daecad85c71bd57
Muhammad Haris <muhammadharis31001@gmail.com>
2026-02-17 11:12:12 +0000

    Simple Age program

--- Day3.py
+++ Day3.py
@@ -1,1 +1,15 @@
-print("hello")
+#Ask user age
+#If age < 13 → Child
+#13–19 → Teenager
+#20–59 → Adult
+#60+ → Senior
+
+age=int(input("Enter Your age :"))
+if age < 13:
+    print("Child")
+elif age >= 13 and age <= 19:
+    print("Teenager") 
+elif age >= 20 and age <= 59:
+    print("Adult")
+else:
+    print("Senior")
