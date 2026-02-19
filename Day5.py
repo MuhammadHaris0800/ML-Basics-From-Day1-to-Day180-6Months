7043e7c4eb4b4e09000bbd73a9ad41a74cb8c529
Muhammad Haris <muhammadharis31001@gmail.com>
2026-02-19 16:46:58 +0000

    Incomplete

---
+++ Day5.py
@@ -0,0 +1,21 @@
+#id=20
+#for i in range (1 , id +1):
+#   print(i)
+
+
+#for i in range (1,6):
+#   print("Row",i,":",end="")
+#   for set in range (5):
+#     print("S", end="")
+#   print()  
+   
+#num=20
+#or i in range (num +1):
+ #      if i %2 ==0:
+ #      print(i)
+        
+  
+#num=20
+#for i in range (num +1):
+#       if i %2 !=0:
+#        print(i)
