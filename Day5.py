ea51fd573194497be8d266d8da217eb1120cf969
Muhammad Haris <muhammadharis31001@gmail.com>
2026-02-20 21:37:25 +0500

    Update Day5.py
    Updated

--- Day5.py
+++ Day5.py
@@ -1,13 +1,13 @@
-#id=20
-#for i in range (1 , id +1):
-#   print(i)
+id=20
+for i in range (1 , id +1):
+   print(i)
 
 
-#for i in range (1,6):
-#   print("Row",i,":",end="")
-#   for set in range (5):
-#     print("S", end="")
-#   print()  
+for i in range (1,6):
+   print("Row",i,":",end="")
+   for set in range (5):
+     print("S", end="")
+    print()  
    
 #num=20
 #or i in range (num +1):
